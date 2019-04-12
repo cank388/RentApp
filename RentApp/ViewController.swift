f34ea4209924c788ec5047b59f350a9251a0357d
cankalender <cankalender.tr@gmail.com>
2019-04-12 15:26:32 +0300

    Initial Commit

---
+++ RentApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RentApp
+//
+//  Created by Can Kalender on 12.04.2019.
+//  Copyright © 2019 Can Kalender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
